4d34da9e4c6308a419cb240dcc3a77d46a83df5e
keinberger <philipp.keinberger@gmail.com>
2024-03-16 21:56:01 +0000

    feat: add correct token addresses

--- nextjs/utils/config.ts
+++ nextjs/utils/config.ts
@@ -1,13 +1,12 @@
+import contracts from "~~/config/contracts";
 import {
   counterConfig,
   poolManagerConfig,
   poolModifyLiquidityTestConfig,
   poolSwapTestConfig,
-  token0Address,
-  token1Address,
 } from "~~/generated/generated";
 
-export const TOKEN_ADDRESSES = [{31337: "0x0165878A594ca255338adfa4d48449f69242Eb8F"},{31337:"0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"}];
+export const TOKEN_ADDRESSES = [{ 31337: contracts.Token0.address }, { 31337: contracts.Token1.address }];
 
 export const DEBUGGABLE_ADDRESSES = [
   { ...counterConfig, name: "Counter" },
