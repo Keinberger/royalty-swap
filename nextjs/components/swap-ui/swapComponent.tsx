f01d90b08d7f08d512b7cad41e786a672eb3dc7e
riemann <aleqvids@gmail.com>
2024-03-16 18:09:45 +0000

    upd

--- nextjs/components/swap-ui/swapComponent.tsx
+++ nextjs/components/swap-ui/swapComponent.tsx
@@ -139,7 +139,7 @@ function SwapComponent() {
       // msg.sender
       encodeAbiParameters(
         [
-          { name: 'x', type: 'address' },
+          { name: 'hookData', type: 'address' },
         ],
         ["0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"]
       ),
